8014c5a01f3d863538bcd59c375f2f402bf3d4de
John Doe <johndoe@example.com>
2021-01-07 12:19:30 -0500

    pdf_to_text

--- imagetotext.py
+++ imagetotext.py
@@ -8,10 +8,8 @@ def ocr_core(filename):
     """
     This function will handle the core OCR processing of images.
     """
-    text = pytesseract.image_to_string(Image.open('/users/salimakazab/desktop/image1.jpg'))  
+    text = pytesseract.image_to_string(Image.open('/users/salimakazab/desktop/image1.jpg'))  #image to text works
     return text
 
 print(ocr_core('/users/salimakazab/desktop/image1.jpg'))
-
-
 
