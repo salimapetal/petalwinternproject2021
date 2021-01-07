b5d1357e80916a9d708e4a75f64bbeff304824e5
John Doe <johndoe@example.com>
2021-01-07 11:06:26 -0500

    init

---
+++ imagetotext.py
@@ -0,0 +1,17 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+def ocr_core(filename):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    text = pytesseract.image_to_string(Image.open('/users/salimakazab/desktop/invoice'))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    return text
+
+print(ocr_core('/users/salimakazab/desktop/invoice'))
+
+
+
