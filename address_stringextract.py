0b6ece95750da3748c6c752708421430ba2a3b5b
Salima Kazab <salima.kazablevine@petalcard.com>
2021-01-08 16:07:49 -0500

    .gitignore

--- address_stringextract.py
+++ address_stringextract.py
@@ -1,9 +1,10 @@
-import imagetotext
-#step1 split up bill in half
-#step2 run regex per half
-
-txt = 
-string.split(separator, maxsplit)
-
-
-
+import re
+text = "Hello, my name is . I live in 329 Yellow Street, Syracuse NY 11346. I have two cats."
+first_half, second_half = text[:len(text)//2], text[len(text)//2:]
+regexp = "(\d{1,10}( \w+){1,10}( ( \w+){1,10})?( \w+){1,10}[,.](( \w+){1,10}(,)? [A-Z]{2}( [0-9]{5})?)?)"
+print(first_half)
+print(second_half)
+address = re.findall(regexp, first_half)
+address2 = re.findall(regexp, second_half)
+print(address)
+print(address2)
