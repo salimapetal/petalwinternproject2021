39566cd0f584f43d21423d7943ddf8ab3a5ae4e4
Salima Kazab <salima.kazablevine@petalcard.com>
2021-01-08 16:09:11 -0500

    split_text_test

--- address_stringextract.py
+++ address_stringextract.py
@@ -1,5 +1,5 @@
 import re
-text = "Hello, my name is . I live in 329 Yellow Street, Syracuse NY 11346. I have two cats."
+text = "Hello, my name is Beata Samel. I live in 329 Cabota Avenue, Copiague NY 11726. My favorite color is red. I have two cats. I live in a house. I like to read"
 first_half, second_half = text[:len(text)//2], text[len(text)//2:]
 regexp = "(\d{1,10}( \w+){1,10}( ( \w+){1,10})?( \w+){1,10}[,.](( \w+){1,10}(,)? [A-Z]{2}( [0-9]{5})?)?)"
 print(first_half)
