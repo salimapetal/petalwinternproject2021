9aec27c99803d3dfc398f1551f8205d59cf95332
justmiah <miah.kirton@petalcard.com>
2021-01-08 10:41:36 -0500

    cond

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -1,20 +1,28 @@
+
 import os
 import ocrmypdf
 import requests
 import pdfplumber
-invoice_pdf = '/Users/salimakazab/Desktop/projects/codeprojects/test6.pdf'
-with pdfplumber.open(invoice_pdf) as pdf:
+
+invoice_pdf = '/Users/miahkirton/Documents/test6.pdf'
+ 
+with pdfplumber.open(invoice_pdf) as pdf: 
     page = pdf.pages[0]
     text = page.extract_text()
     print(text)
-    print(type(text))
-     #shouldn't return anything bc it's not an ocr pdf
-os.system(f'ocrmypdf {invoice_pdf} output.pdf') #invoice_pdf should not be a string?
-with pdfplumber.open('output.pdf') as pdf:
-    page = pdf.pages[0]
-    text = page.extract_text(x_tolerance=2)
+    #print(type(text))
+     
+if text == None :
+    os.system(f'ocrmypdf {invoice_pdf} output.pdf') 
+
+    with pdfplumber.open('output.pdf') as pdf:
+        page = pdf.pages[0]
+        text = page.extract_text(x_tolerance=2)
+        print(text)
+else: 
     print(text)
-lines = text.split('/n')
+
+lines = text.split('/n') 
 import re
 street_address = re.search(r'\d{1,4} [\w\s]{1,20}(?:street|st|avenue|ave|road|rd|highway|hwy|square|sq|trail|trl|drive|dr|court|ct|parkway|pkwy|circle|cir|boulevard|blvd)\W?(?=\s|$)', text)
 print(street_address)
