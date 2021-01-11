12eac91ffd644522a3b9bc606737698463f1798d
Salima Kazab <salima.kazablevine@petalcard.com>
2021-01-11 11:02:03 -0500

    ZhengUtilityBillUpdate

--- imagetotext.py
+++ imagetotext.py
@@ -8,11 +8,8 @@ def ocr_core(filename):
     """
     This function will handle the core OCR processing of images.
     """
-    text = pytesseract.image_to_string(Image.open('/Users/salimakazab/Desktop/petalwinternproject21/petalwinternproject2021/files/utilitybill1.jpg'))  #image to text works
+    text = pytesseract.image_to_string(Image.open('/Users/salimakazab/Desktop/petalwinternproject21/petalwinternproject2021/files/ZhengUtilityBill.jpg'))  #image to text works
     return text
 
-print(ocr_core('/Users/salimakazab/Desktop/petalwinternproject21/petalwinternproject2021/files/utilitybill1.jpg'))
+print(ocr_core('/Users/salimakazab/Desktop/petalwinternproject21/petalwinternproject2021/files/ZhengUtilityBill.jpg'))
 
-#f = open("text.txt", "w")
-#f.write(ocr_core('/Users/salimakazab/Desktop/petalwinternproject21/petalwinternproject2021/files/utilitybill1.jpg'))
-#f.close()
