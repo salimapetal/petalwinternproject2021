9df2eaadcff28e5294d0d0d9b82f221b049b7993
Salima Kazab <johndoe@example.com>
2021-01-14 17:29:46 -0500

    serverfixed

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-@app.route('/start') #@app.route('/', methods=['POST']) could not be done error : method not allowed
+@app.route('/') #@app.route('/', methods=['POST']) could not be done error : method not allowed
 def upload_form():
     return render_template('index.html')
 @app.route('/success')
