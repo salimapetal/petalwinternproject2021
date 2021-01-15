6fc75b71a9e4ef39882d42427e79574c83765cb6
Salima Kazab <johndoe@example.com>
2021-01-15 17:28:19 -0500

    uploads_nosubfolder

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.secret_key = "secret key"
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 path = os.getcwd()
 # file Upload
-UPLOAD_FOLDER = os.path.join(path, 'uploads')
+UPLOAD_FOLDER = os.path.join(path)
 if not os.path.isdir(UPLOAD_FOLDER):
     os.mkdir(UPLOAD_FOLDER)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
