063a6e6b1d0bf03ddd9b7996ef1b2e029db0af7e
Salima Kazab <johndoe@example.com>
2021-01-15 17:08:21 -0500

    upload_adressext_works

--- iteration.py
+++ iteration.py
@@ -2,7 +2,7 @@ import os
 path="/Users/salimakazab/Desktop/petalwinternproject2021/uploads"
 os.chdir(path)
 for filename in os.listdir(path):
-    if filename.endswith(".txt, .pdf, .png, .jpg, .jpeg, .gif"):
-        f = open(filename)
+   if filename.endswith(".txt, .pdf, .png, .jpg, .jpeg, .gif"):
+       f = open(filename)
 filename = path + "/" + filename
 print(filename)
